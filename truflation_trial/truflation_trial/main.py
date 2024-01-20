05909b28a9a1061b34e1e592dd7b91e5317e7f34
root <root@ip-172-31-22-148.ap-southeast-1.compute.internal>
2024-01-20 15:31:59 +0000

    add Feed to SQLiteDB

--- truflation_trial/truflation_trial/main.py
+++ truflation_trial/truflation_trial/main.py
@@ -8,11 +8,38 @@
 # import sqlalchemy
 # from sqlalchemy.orm import Session
 import yfinance as yf
+import pandas as pd
+from datetime import datetime
 # from typing import List
-def start():
+
+from database import Base, engine, get_db, SessionLocal
+from schemas import HistorySchema
+# from models import History
+
+def add_Feed():
+    db = SessionLocal()
     ticker = yf.Ticker("LITH-USD")
-    daily = ticker.history()
-    print(daily)
-    
+    h = ticker.history()
+    df = h.Close.reset_index()
+    df["created_at"] = pd.Timestamp(datetime.now())
+    df = df.round({"Close": 6})
+    df.rename(columns={"Date": "date_value", "Close": "value"}, inplace=True)
+    df_records = df.to_dict(orient="records")
+    for record in df_records:
+        record["date_value"] = pd.Timestamp(record["date_value"])
+        record["created_at"] = pd.Timestamp(record["created_at"])
+        record["value"] = round(float(record["value"]), 6)
+        # Create an instance of HistorySchema for each record
+        history_schema = HistorySchema(**record)
+
+        # Add the instance to the database session
+        db.add(history_schema)
+
+    # Commit changes to the database
+    db.commit()
+    db.close()
+
+Base.metadata.create_all(bind=engine)
+
 if __name__ == '__main__': 
-    start()
+    add_Feed()
--- truflation_trial/truflation_trial/schemas.py
+++ truflation_trial/truflation_trial/schemas.py
@@ -1,24 +1,11 @@
 from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String, Numeric
 from sqlalchemy.orm import relationship
-from backend.database import Base
-
-
-class SymbolSchema(Base):
-    __tablename__ = "symbols"
-    id = Column(Integer, primary_key=True, index=True)
-    symbol = Column(String, unique=True, index=True)
-    name = Column(String)
-    exchange = Column(String)
-    prices = relationship("HistorySchema")
+from database import Base
 
 
 class HistorySchema(Base):
     __tablename__ = "history"
     id = Column(Integer, primary_key=True, index=True)
-    symbol_id = Column(Integer, ForeignKey("symbols.id"))
-    date = Column(Date)
-    open = Column(Numeric(10, 2))
-    high = Column(Numeric(10, 2))
-    low = Column(Numeric(10, 2))
-    close = Column(Numeric(10, 2))
-    volume = Column(Integer)
+    date_value = Column(Date)
+    value = Column(Numeric(10, 6))
+    created_at = Column(Date)
